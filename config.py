18c65ce61766b334a9ba2cf40b226d574b28cfc8
Ferril <ferril.darkdiver@gmail.com>
2019-12-06 15:28:17 +0300

    Redacted login page

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    DEBUG = os.environ.get('DEBUG') or True
+    FLASK_ENV = os.environ.get('FLASK_ENV') or 'development'
