68e7be2f766d095a34fb12158bac11a8baa0cc79
Ferril <ferril.darkdiver@gmail.com>
2019-12-09 06:55:10 +0300

    Added sqlalchemy settings and User model

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = os.environ.get('DEBUG') or True
     FLASK_ENV = os.environ.get('FLASK_ENV') or 'development'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
